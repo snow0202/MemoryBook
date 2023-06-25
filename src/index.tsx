7eecf71cf34fc94ad4b954d838ffac9c380f63b4
snow <surf.snow0202@gmail.com>
2023-06-25 22:31:51 +0900

    ルーターの設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App';
+import { Login } from './Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const handleLogin = (email: string, password: string) => {
+  console.log('ログインされたメールアドレス:', email, 'とパスワード:', password);
+};
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+      <Route path="/login" element={<Login onSubmit={handleLogin} />} />
+        <Route path="/App" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
