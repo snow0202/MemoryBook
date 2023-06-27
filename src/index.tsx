1e928856a4d7ed7f99ed39dbd0434307d4144d1a
snow <surf.snow0202@gmail.com>
2023-06-27 22:45:33 +0900

    ルーターの初期設定とボタンの画面遷移設定

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App';
-import { Login } from './Login';
+import { Login } from './Components/Login/Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 const handleLogin = (email: string, password: string) => {
   console.log('ログインされたメールアドレス:', email, 'とパスワード:', password);
 };
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <Routes>
-      <Route path="/login" element={<Login onSubmit={handleLogin} />} />
+        <Route path="/login" element={<Login onSubmit={handleLogin} />} />
         <Route path="/App" element={<App />} />
       </Routes>
     </BrowserRouter>
