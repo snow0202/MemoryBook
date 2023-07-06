03fc75e6764d95e329006d5234f3e57455e4b50c
snow <surf.snow0202@gmail.com>
2023-07-06 23:07:25 +0900

    ログイン画面認証設定

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -2,37 +2,46 @@ import { useState } from "react";
 import Style from './Login.module.css';
 import { LoginEvent } from "./LoginEvent";
 
-interface LoginProps {
-    onSubmit: (name: string, password: string) => void;
-}
-
-export const Login: React.FC<LoginProps> = ({ onSubmit }) => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      onSubmit(name, password);
-    };
+export const Login: React.FC = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
 return (
-  <div className={Style['change-img-anim']}>
-      <form className={Style.form} onSubmit={handleSubmit}>
-          <h1 className={Style.head}>おもいでへようこそ✨</h1>
-          <div className={Style.label}>
-            <label className={Style.labelName}>
-              おなまえ {''}
-              <input className={Style.textName} type="name" value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <div>{''}</div>
-            <label className={Style.labelPass}>
-              パスワード {''}
-              <input className={Style.textPass} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <div>{''}</div>
-            <LoginEvent className={Style.loginButton} />
-            </div>
-      </form>
+    <div className={Style['change-img-anim']}>
+      <header className={Style.head}>おもいでへようこそ</header>
+      <div className={Style.label}>
+          <label className={Style.labelName}>
+            おなまえ {''}
+            <input 
+              className={Style.textName} 
+              type="name" 
+              value={name} 
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setName(e.target.value)
+              }}
+            />
+          </label>
+          <div>{''}</div>
+          <label className={Style.labelPass}>
+            パスワード {''}
+            <input 
+                className={Style.textPass} 
+                type="password" 
+                value={password} 
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPassword(e.target.value)
+                }}
+               />
+          </label>
+          <div>{''}</div>
+          <LoginEvent 
+             className={Style.loginButton} 
+            isLogin={isLogin}
+            name={name}
+            password={password}
+          />
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -6,23 +6,16 @@ import App from './App';
 import { Login } from './Components/Login/Login';
 import { UploadPage } from './Components/UploadPage/UploadPage'
 import { Contact } from './Components/Contact/Contact';
-import cat1 from './img/cat1.jpg'
-import cat2 from './img/cat2.jpg'
-import cat3 from './img/cat3.jpg'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const handleLogin = (name: string, password: string) => {
-  console.log('ログインされたメールアドレス:', name, 'とパスワード:', password);
-};
-
 root.render(
   <React.StrictMode>
     <BrowserRouter basename='/'>
       <Routes>
-        <Route path="/login" element={<Login onSubmit={handleLogin} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/App" element={<App />} />
         <Route path="/UploadPage" element={<UploadPage />} />
         <Route path="/Contact" element={<Contact />} />
